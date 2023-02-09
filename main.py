d694a4bee7af06e32b8dc15029d908b88667ed38
VA <dclass096@gmail.com>
2023-02-09 08:00:43 +0500

    2

--- main.py
+++ main.py
@@ -20,15 +20,27 @@ users = get_users_list()  # никнейм пользователя - ключ :
 
 # print (data)
 
+user_requests = dict()  # user_id - ключ : значение - количество запросов пользователя
+
+
+
 @bot.message_handler(commands=["start"], content_types=["text"])  # Стартовая функция
 def start_message(message):
     cur_buttons = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
     for i in currencies:
         cur_buttons.add(telebot.types.KeyboardButton(i))
-        print(i)
+        # print(i)
     bot.send_message(message.chat.id, f"Здравствуйте {message.from_user.first_name}, я бот валютчик. "
                                       f"Чтобы узнать все что я могу напишите /help"
                                       f" или нажмите на кнопку help", reply_markup=cur_buttons)
+
+
+@bot.message_handler(commands=["help"], content_types=["text"])
+def help_message(message):
+    bot.send_message(message.chat.id, "Чтобы начать работать со мной, вы можете нажать на кнопку рядом с полем ввода"
+                                      " и тогда вам будет доступен список популярных валют, если вы хотите узнать"
+                                      " данные по другим валютам, напишите мне стандартное 3х буквенное обозначение"
+                                      " валюту - Пример:\nTRY - Турецкая лира")
 
 
 @bot.message_handler(content_types=["text"])
@@ -40,17 +52,18 @@ def echo_message(message):
         bot.send_message(message.chat.id,  # 1 сикль -  29 кнатов; 1 галлеон - 17 сиклей;
                          f"За дополнительной информацией\nпросьба обратиться в банк " +
                          f"[Гринготтс](harrypotter.fandom.com/ru/wiki/Гринготтс)", parse_mode="Markdown")
-    if message.text in currencies:
-        bot.send_message(message.chat.id, f"{message.text} - "
-                                          f"{data['Valute'][currencies[message.text].upper()]['Value']}")
+    if (message.text != "/help"):
+        if (message.text in currencies):
+            bot.send_message(message.chat.id, f"{message.text} - "
+                                              f"{data['Valute'][currencies[message.text].upper()]['Value']}")
+        else:
+            try:
+                bot.send_message(message.chat.id, f"1 {message.text} - "
+                                                  f"{data['Valute'][message.text.upper()]['Value']} RUB")
 
 
-@bot.message_handler(commands=["help"], content_types=["text"])
-def help_message(message):
-    bot.send_message(message.chat.id, "Чтобы начать работать со мной, вы можете нажать на кнопку рядом с полем ввода"
-                                      " и тогда вам будет доступен список популярных валют, если вы хотите узнать"
-                                      "данные по другим валютам, напишите мне стандартное 3х буквенное обозначение"
-                                      "валюту - Пример:\nTRY - Турецкая лира")
+            except KeyError:
+                bot.send_message(message.chat.id, "Ошибка, такой валюты не существует")
 
 
 bot.polling(none_stop=True)
